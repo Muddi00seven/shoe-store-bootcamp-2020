1f158d47e3d95abfb0262c2ebd65ce381f8dc31d
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-07-11 19:20:39 +0500

    build 0.1
    GLOBAL PROVIDER ADDED

--- src/Components/GlobalContext/GlobalContext.js
+++ src/Components/GlobalContext/GlobalContext.js
@@ -1,5 +1,6 @@
 import React, {createContext , useReducer} from 'react'
 import { shoesData } from '../Products/ShoesCard';
+import GlobalReducer from './GlobalReducer'
 
 const initialState = {
     shoesData,
@@ -11,10 +12,57 @@ const initialState = {
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({children}) => {
+let [state , dispatch] = useReducer(GlobalReducer , initialState)
+
+    const addItem = (product) => {
+        dispatch({
+            type: 'ADD_TO_CART',
+            payload : product
+        })
+    } 
+
+    const plus = (id) => {
+        dispatch({
+            type: 'INCREASE',
+            payload : id
+        })
+    } 
+
+    const minus = (id) => {
+        dispatch({
+            type: 'DECREASE' ,
+            payload : id
+        })
+    } 
+
+    const remove = (id) => {
+        dispatch({
+            type: 'REMOVE',
+            payload : id
+        })
+    } 
+
+    const checkOut = () => {
+        dispatch({
+            type: 'CHECKOUT'
+        })
+    } 
+
+
 
     return(
-        <di>
+       <GlobalContext.Provider value={{
+           shoesData:state.shoesData,
+           cart:state.cart,
+           addItem,
+           plus,
+           minus,
+           remove,
+           checkOut
+       }}>
 
-        </di>
+
+{children}
+       </GlobalContext.Provider>
     )
 }
